a229107baa65c9830d0b32afc5387e9ca456ec6f
Cecilia González <ceciliasoto200309@gmail.com>
2023-09-18 09:20:18 -0500

    Commit update

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* ---MOSTRAR MENU Y OCULTAR (HIDDEN)--- */
+const navMenu = document.getElementById('nav-menu'),
+    navToggle=document.getElementById('nav-toggle'),
+    navClose=document.getElementById('nav-close')
+
+/* ---MOSTRAR MENU--- */
+/* ---Validar si existe constante--- */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* ---OCULTAR MENU--- */
+/* ---Validar si existe constante--- */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/* ---REMOVER MENU MOVIL--- */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
