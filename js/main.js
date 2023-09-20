37e8026c572a02382f91bd249ec785e629221714
Cecilia González <ceciliasoto200309@gmail.com>
2023-09-20 10:03:35 -0500

    Sección de nivel de tecnicas y formacion academica

--- js/main.js
+++ js/main.js
@@ -28,3 +28,42 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* ---ACORDEON DE HABILIDADES--- */
+const skillsContent=document.getElementsByClassName('skills__content'),
+    skillsHeader=document.querySelectorAll('.skills__header')
+
+    function toggleSkills(){
+        let itemClass=this.parentNode.className
+        for(i=0; i<skillsContent.length;i++){
+            skillsContent[i].className='skills__content skills__close'
+        }
+        if(itemClass==='skills__content skills__close'){
+            this.parentNode.className='skills__content skilss__open'
+        }
+    }
+
+    skillsHeader.forEach((el)=>{
+        el.addEventListener('click', toggleSkills)
+    })
+
+/* ------ */
+const tabs = document.querySelectorAll('[data-target]'),
+tabContents = document.querySelectorAll('[data-content]')
+
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContents=>{
+            tabContents.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
